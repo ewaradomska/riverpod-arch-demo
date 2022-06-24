6efea293e80561fab4835308a5444be0255874b2
Ewa Radomska <ewa.radomska@gmail.com>
2022-06-24 21:09:56 +0200

    add firebase (no creds)

--- lib/main.dart
+++ lib/main.dart
@@ -1,8 +1,14 @@
 import 'package:demo/memes/pages/meme_list_page.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
+import 'package:firebase_core/firebase_core.dart';
+//you need to add your firebase options
+//import 'firebase_options.dart';
 
-void main() {
+void main() async {
+  await Firebase.initializeApp(
+      //  options: DefaultFirebaseOptions.currentPlatform,
+      );
   runApp(const MyApp());
 }
 
@@ -16,15 +22,6 @@ class MyApp extends StatelessWidget {
       child: MaterialApp(
           title: 'Flutter Demo',
           theme: ThemeData(
-            // This is the theme of your application.
-            //
-            // Try running your application with "flutter run". You'll see the
-            // application has a blue toolbar. Then, without quitting the app, try
-            // changing the primarySwatch below to Colors.green and then invoke
-            // "hot reload" (press "r" in the console where you ran "flutter run",
-            // or simply save your changes to "hot reload" in a Flutter IDE).
-            // Notice that the counter didn't reset back to zero; the application
-            // is not restarted.
             primarySwatch: Colors.blue,
           ),
           home: const MemeListPage()),
