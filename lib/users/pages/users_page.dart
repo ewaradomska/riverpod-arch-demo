0afadd894155c1d3226d27092bc11c78d6f8477f
Ewa Radomska <ewa.radomska@gmail.com>
2022-06-24 21:42:33 +0200

    users page

--- lib/users/pages/users_page.dart
+++ lib/users/pages/users_page.dart
@@ -1,3 +1,7 @@
+import 'package:demo/core/widgets/error.dart';
+import 'package:demo/users/controllers/users_state.dart';
+import 'package:demo/users/controllers/users_state_notifier.dart';
+import 'package:demo/users/widgets/user_tile.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 
@@ -6,9 +10,38 @@ class UsersPage extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Consumer(builder: (context, watch, child) {
-      //  final state = ...
-      return Container();
-    });
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('Users'),
+      ),
+      body: Consumer(
+        builder: (context, ref, child) {
+          final state = ref.watch(usersStateNotifierProvider);
+          if (state == const UsersState.initial()) {
+            WidgetsBinding.instance!.addPostFrameCallback((timeStamp) {
+              ref.read(usersStateNotifierProvider.notifier).getUsers();
+            });
+          }
+          return state.when(
+            initial: () => const Center(
+              child: CircularProgressIndicator.adaptive(),
+            ),
+            loading: () => const Center(
+              child: CircularProgressIndicator.adaptive(),
+            ),
+            users: (users) => ListView.builder(
+              itemCount: users.length,
+              itemBuilder: (context, index) => UserTile(
+                  name: users[index].name,
+                  surname: users[index].surname,
+                  age: users[index].age),
+            ),
+            error: (error) => Center(
+              child: ErrorInfo(error: error),
+            ),
+          );
+        },
+      ),
+    );
   }
 }
